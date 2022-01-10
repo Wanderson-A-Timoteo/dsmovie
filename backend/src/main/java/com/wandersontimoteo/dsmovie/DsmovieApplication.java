005958de5280021531c480a1476cbf5299ed0666
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-01-10 12:05:01 -0400

    Project created

---
+++ backend/src/main/java/com/wandersontimoteo/dsmovie/DsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.wandersontimoteo.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wandersontimoteo/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandersontimoteo.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
