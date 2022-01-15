3c1ca8c4c36c2c4722e11154ed85f12db0a717c4
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-01-15 17:06:15 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+  return (
+    <div>
+      <h1>Página de Formulário</h1>
+    </div>
+  );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing() {
+  return (
+    <div>
+      <h1>Página de Listagem</h1>
+    </div>
+  );
+}
+
+export default Listing;
